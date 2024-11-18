297494eaf8d26307442666ba84adbc6688bb8c00
André Picado <andre.picado@tecnico.ulisboa.pt>
2024-11-18 12:30:07 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule, MatTooltipModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'portfolio';
+
+  faDownload = faDownload;
+
+  linkedinUrl = 'https://www.linkedin.com/in/andre-picado/';
+  downloadResume() {
+    console.log('downloadResumeCalled');
+    window.open('assets/andre_picado_resume.pdf', '_blank');
+  }
+}
